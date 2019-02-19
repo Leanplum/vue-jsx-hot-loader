0e1fa3d1a52f3b49186b19ba6d0b7c4f4f34a2a9
Alex Gyoshev <aleksander.gyoshev@leanplum.com>
2019-02-19 17:05:22 +0200

    feat: enable HMR for multiple exports
    Approach: the code loops through all exports from the reloaded module and calls the Vue API to accept each component.
    
    Note: super.name is 'VueComponent' for mixins.

--- src/api.js
+++ src/api.js
@@ -27,6 +27,11 @@ const findComponent = ({ ctx, module }) => {
     return module.exports.__esModule ? module.exports.default : module.exports;
 };
 
+const isVueComponent = component => {
+    const name = _.get(component, 'super.name');
+    return name === 'Vue' || name === 'VueComponent';
+};
+
 module.exports = ({ Vue, ctx, module, hotId }) => {
     // Make the API aware of the Vue that you are using.
     // Also checks compatibility.
@@ -35,28 +40,38 @@ module.exports = ({ Vue, ctx, module, hotId }) => {
     // Accept the hot replacement.
     module.hot.accept();
 
-    // Retrieve the exported component. Handle ES and CJS modules as well as
-    // untransformed ES modules (env/es2015 preset with modules: false).
-    const component = findComponent({ ctx, module });
-    if (!component || _.isEmpty(component)) {
-        return;
-    }
+    const reloadComponent = (id, component) => {
+        // Serialize everything but the render function.
+        // We'll use it to decide if we need to reload or rerender.
+        const serialized = serialize(_.omit(component, ["render"]));
 
-    // Serialize everything but the render function.
-    // We'll use it to decide if we need to reload or rerender.
-    const serialized = serialize(_.omit(component, ["render"]));
+        if (!module.hot.data) {
+            // If no data, we need to create the record.
+            api.createRecord(id, component);
+        } else if (cache[id] === serialized) {
+            // Rerender only since the component hasn't changed.
+            api.rerender(id, component);
+        } else {
+            // Reload the component.
+            api.reload(id, component);
+        }
 
-    if (!module.hot.data) {
-        // If no data, we need to create the record.
-        api.createRecord(hotId, component);
-    } else if (cache[hotId] === serialized) {
-        // Rerender only since the component hasn't changed.
-        api.rerender(hotId, component);
+        // Save the serialized component to the cache.
+        cache[id] = serialized;
+    };
+
+
+    if (module.exports.__esModule && !module.exports.default) {
+        // Loop over all module exports, reload components
+        Object.values(module.exports)
+            .filter(isVueComponent)
+            .forEach(component => reloadComponent(`${hotId}_${component.name}`, component));
     } else {
-        // Reload the component.
-        api.reload(hotId, component);
+        // Retrieve the exported component. Handle ES and CJS modules as well as
+        // untransformed ES modules (env/es2015 preset with modules: false).
+        const component = findComponent({ ctx, module });
+        if (component && !_.isEmpty(component)) {
+            reloadComponent(hotId, component);
+        }
     }
-
-    // Save the serialized component to the cache.
-    cache[hotId] = serialized;
 };
